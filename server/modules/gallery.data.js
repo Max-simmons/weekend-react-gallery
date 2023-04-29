ad4b0931442103fd2ca1431bf7e072b3ae07aec5
Max Simmons <maxsimmons20@gmail.com>
2023-04-29 11:22:26 -0500

    added images to the server

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,12 +1,27 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', 
-    description: 'Photo of a goat taken at Glacier National Park.', 
+    { id: 1, path: 'images/komodoDragon.jpg', 
+    description: 'Komodo Dragon: Prey includes monkeys, water buffalo, lizards, snakes, and small rodents.', 
     likes: 0 },
-    { id: 2, path: '', description: '', likes: 0 },
-    { id: 3, path: '', description: '', likes: 0 },
-    { id: 4, path: '', description: '', likes: 0 },
-    { id: 5, path: '', description: '', likes: 0 },
-    { id: 6, path: '', description: '', likes: 0 }
+
+    { id: 2, path: 'images/Gray-wolf.jpeg', 
+    description: 'Gray Wolf: Prey includes deer, bison, moose, beaver, rabbit, and fish',
+     likes: 0 },
+
+    { id: 3, path: 'images/muskie.jpeg', 
+    description: 'Muskellunge: Prey includes fish, ducklings, frogs, muskrats, and mice ',
+     likes: 0 },
+
+    { id: 4, path: 'images/Owl.jpeg', 
+    description: 'Barn Owl: Prey includes rats, mice, voles, birds, lizards, and rabbits', 
+    likes: 0 },
+
+    { id: 5, path: 'images/Tiger.jpeg', 
+    description: 'Tiger: Prey includes ungulates, deer, wild boar, monkeys, and even crocodile.',
+     likes: 0 },
+
+    { id: 6, path: 'images/Alligator.jpeg', 
+    description: 'American Alligator: Prey includes fish, snails, birds, mammals, and frogs', 
+    likes: 0 }
 ];
 
 
