6762315688acb2cb3b8344d7fba81409933132a3
Max Simmons <maxsimmons20@gmail.com>
2023-04-29 12:49:22 -0500

    added the GalleryList component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios'; 
+import GalleryList from '../GalleryList/GalleryList';
+
 
 
 
 
 function App() {
+
+  const [galleryImages, setGalleryImages] = useState([]);
+
+  const fetchImages = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then((res) => {
+      setGalleryImages(res.data);
+      console.log('Got our images');
+    }).catch((err) => {
+      console.log('Couldnt get images', err)
+    })
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
+        <GalleryList 
+        fetchImages = {fetchImages} 
+        />
         <img src="images/goat_small.jpg"/>
       </div>
     );
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,1 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import GalleryItem from '../GalleryItem/GalleryItem';
+
+
+
+function GalleryList(props) {
+    
+    return (
+        <ul>
+        {props.galleryImages.map(image => {
+            return (
+                <GalleryItem
+                key = {image.id}
+                image = {image.path}
+                description = {image.descrption}
+                likes = {image.likes}  
+                fetchImage  = {fetchImage}
+                />
+
+            )
+        })
+    }
+            
+        </ul>
+    )
+
+}
+
 export default GalleryList;
